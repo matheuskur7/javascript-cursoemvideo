7688298838f7e03344950496f44ac81ab261cdb9
MatheusKur <60073283+matheuskur7@users.noreply.github.com>
2024-05-28 20:19:38 -0300

    ex001 Resolvido!

--- exercícios/ex001/scripts/script.js
+++ exercícios/ex001/scripts/script.js
@@ -1,8 +1,7 @@
-verificaHora()
 
-function verificaHora () {
+function carregar () {
     var data = new Date()
-    var hora = 2 //Number(data.getHours())
+    var hora = Number(data.getHours())
     console.log(hora)
 
     var texto = document.getElementById('texto')
@@ -13,19 +12,19 @@ function verificaHora () {
         console.log('Bom dia')
         body.style.backgroundColor = 'lightblue'
         texto.innerText = 'Agora são ' + hora + ' horas'
-        imagem.style.background = 'url(../images/afternoon-200.jpg);'
+        imagem.src = 'images/morning-200.jpg'
     } else {
         if (hora < 18 && hora >= 12) {
         console.log('Boa tarde')
         body.style.backgroundColor = 'lightyellow'
         texto.innerText = 'Agora são ' + hora + ' horas'
-        imagem.style.background = 'url(../images/afternoon-200.jpg);'
+        imagem.src = 'images/afternoon-200.jpg'
         } else {
             if (hora <= 23 && hora >= 0)
             console.log('Boa noite')
             body.style.backgroundColor = 'darkcyan'
             texto.innerText = 'Agora são ' + hora + ' horas'
-            imagem.style.background = 'url(../images/night-200.jpg);'
+            imagem.src = 'images/night-200.jpg'
         }
     }
 }
